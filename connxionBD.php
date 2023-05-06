628816983aa7bd4f53310cf2bf448c734ed9729b
aliou <diallomohamadaly@gmail.com>
2023-05-06 16:01:42 +0000

    first commit

--- connxionBD.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servname = 'localhost';
-    $dbname = 'projetGestionEtablissement';
-    $user = 'root';
-    $pass = '78122781Mad.';
-    $dbco = new PDO("mysql:host=$servname;dbname=$dbname", $user, $pass);
-    $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
-?>
